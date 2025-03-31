c10b3b6f760ee7647db88890c4269e3188833156
Murad <seferov.murad.98@gmail.com>
2025-03-31 20:22:33 +0400

    'write_views_and_urls_products'

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from products.models import Category, Product, Review
+from products.serializers import ProductSerializer, CategorySerializer, ReviewSerializer
+from products.filters import ProductFilter
+from rest_framework.filters import OrderingFilter
+import django_filters
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, OrderingFilter]
+    filterset_class = ProductFilter
+    ordering_fields = ['price', 'created_at']
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def perform_create(self, serializer):
+        serializer.save(
+            user = self.request.user,
+            product_id = self.kwargs['product_pk']
+        )
